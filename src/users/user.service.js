96783a6a793a5f87eeca5a6a8a52556d1532e799
Carlos Rubio <calitod91@gmail.com>
2023-11-01 20:38:47 -0600

    Agregando user service en endpoint users

--- src/users/user.service.js
+++ src/users/user.service.js
@@ -1,1 +1,23 @@
 // Aqui va la logica de consultas a la base de datos
+import {StatusCodes} from 'http-status-codes'
+import userSchema from "./user.schema.js"
+
+export class UserService{
+  async create(user){
+    const {error} = userSchema.validate(user)
+
+    if (error !== undefined){
+      return {
+        statusCode: StatusCodes.BAD_REQUEST,
+        message: `Hubo un error de validación al actualizar el usuario: ${error.message}.`,
+        entity: null
+      }
+    }
+
+    return {
+      statusCode: StatusCodes.OK,
+      message: 'Usuario creado con éxito!.',
+      entity: {user: 'User created'} // Aqui iria la data devuelta por la DB
+    };
+  }
+}
